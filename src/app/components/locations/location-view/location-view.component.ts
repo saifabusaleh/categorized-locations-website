33a13d1511f0fdefa194dd1cf4d3004dbdc86a79
Saif <saif.as7777@gmail.com>
2020-05-01 00:59:36 +0300

    update translation text

--- src/app/components/locations/location-view/location-view.component.ts
+++ src/app/components/locations/location-view/location-view.component.ts
@@ -1,4 +1,4 @@
-import { LocationResponse, LocationStatusEnum } from '@models/location-response';
+import { LocationStatusEnum } from '@models/location-response';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LocationService } from '@services/location/location.service';
